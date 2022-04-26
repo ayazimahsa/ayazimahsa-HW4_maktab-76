3d4342f094ad926811d2d068ae910df3cb87d698
ayazimahsa <mahsaayazii73@gmail.com>
2022-04-26 21:45:28 +0430

    hw5

--- hw5.js
+++ hw5.js
@@ -1,24 +1,11 @@
-function merge_array(array1, array2) {
-    let result_array = [];
-    let arr = array1.concat(array2);
-    let len = arr.length;
-    let assoc = {};
-
-    while (len--) {
-        let item = arr[len];
-
-        if (!assoc[item]) {
-            result_array.unshift(item);
-            assoc[item] = true;
-        }
-    }
-
-    return result_array;
+function MergeArr(ar1, ar2) {
+    // merge 2 arraye ba rest
+    let arr = [...ar1, ...ar2];
+    // hazfe duplicate ha ba new set
+    let uniqueArr = [...new Set(arr)];
+    console.log(uniqueArr);
 }
+const array1 = [1, 2, 3];
+const array2 = [2, 30, 1];
+MergeArr(array1, array2);
 
-
-let array1 = [1, 2, 3];
-
-let array2 = [2, 30, 1];
-
-console.log(merge_array(array1, array2));
