7735e4460be9aeaf8c4e1a2d645f1f755bb0e61f
ayazimahsa <mahsaayazii73@gmail.com>
2022-04-26 21:48:02 +0430

    cw2

--- hw2.js
+++ hw2.js
@@ -1,18 +1,18 @@
-// let arr = [1, 2, 3, 4, 5, 6]
+//   let arr = [1, 2, 3, 4, 5, 6]
 
-// function add(arr) {
-//     if (arr.length === 1) return arr[0]
-//     return arr[0] + add(arr.slice(1))
-// }
-// console.log(add(arr))
-
-// let array = [1, 2, 3, 4, 5];
-
-//     let sum = array.reduce(function(a, b){
-//         return a + b;
-//     }, 0);
-
-//     console.log(sum); 
+//     function add(arr) {
+//         if (arr.length === 1) return arr[0]
+//         return arr[0] + add(arr.slice(1))
+//     }
+//     console.log(add(arr))
+    
+//     let array = [1, 2, 3, 4, 5];
+    
+//         let sum = array.reduce(function(a, b){
+//             return a + b;
+//         }, 0);
+    
+//         console.log(sum);   
 
 let array = [1, 2, 3, 4, 5, 6];
 let sum = 0;
